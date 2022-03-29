0907c8764e5b273f1f4d7f5c1e0465507c0b1ff8
Alba Maria Huertas Dominguez <ahuertasdominguez@gmail.com>
2022-03-29 15:43:46 +0200

    changes

--- src/pages/we/index.jsx
+++ src/pages/we/index.jsx
@@ -2,6 +2,7 @@ import { useTranslation } from 'react-i18next';
 import { IdiomContext } from "../../context/createcontext";
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Card from 'react-bootstrap/Card';
 import Carousel from 'react-bootstrap/Carousel'
 import Container from 'react-bootstrap/Container';
 import { useContext } from 'react';
@@ -9,7 +10,7 @@ import './style.css';
 import cerve1 from '../../assets/img/cerve1.jpg';
 import cerve2 from '../../assets/img/cerve2.jpg';
 import cerve3 from '../../assets/img/cerve3.jpg';
-import cerveza4 from'../../assets/img/cerveza4.jpg';
+import cerveza4 from '../../assets/img/cerveza4.jpg';
 
 
 function WeWho() {
@@ -21,92 +22,87 @@ function WeWho() {
 
     return (
 
-        <Container style={{color:'black', backgroundColor: 'rgba(0,0,0,0.8)', minHeight: '100vh', fontFamily:'highv'}} fluid>
-            <Row className="d-flex flex-wrap justify-content-center">
+        // <Container style={{ color: 'black', backgroundColor: 'rgba(0,0,0,0.8)', minHeight: '100vh', fontFamily: 'highv' }} className="d-flex justify-content-between" fluid>
+             <Row style={{ color: 'black', backgroundColor: 'rgba(0,0,0,0.8)', minHeight: '100vh', fontFamily: 'highv', fontSize:'18px' }}> 
 
 
-                <Col xs={12} m={10} lg={9} xxl={8}>
+             <Col style={{padding:'0px'}}xs={12} sm={12} md={8} lg={7} xxl={7}> 
 
-                    <Carousel className='m-5' fade>
-                        <Carousel.Item>
-                            <img
-                                className="d-block w-100"
-                                src={cerve3}
-                                alt="First slide"
-                            />
-                            <Carousel.Caption>
-                                <h4>{t("who.w2")}</h4>
-                                <h4>{t("who.w3")}</h4>
-                                <h4>{t("who.w4")}</h4>
-                            </Carousel.Caption>
-                        </Carousel.Item>
-                        <Carousel.Item >
-                            <img
-                                className="d-block w-100"
-                                src={cerve2}
-                                alt="Second slide"
-                            />
+                <Carousel style={{margin:'0px', paddingTop:'2rem'}} fade>
+                    <Carousel.Item>
+                        <img
+                            className="d-block w-100"
+                            src={cerve3}
+                            alt="First slide"
+                        />
+                        <Carousel.Caption>
 
-                            <Carousel.Caption style={{color:'#fbbc5d'}}>
-                                <h4>{t("who.w5")}</h4>
-                                <h4>{t("who.w6")}</h4>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                    <Carousel.Item >
+                        <img
+                            className="d-block w-100"
+                            src={cerve2}
+                            alt="Second slide"
+                        />
 
-                            </Carousel.Caption>
-                        </Carousel.Item>
-                        <Carousel.Item>
-                            <img
-                                className="d-block w-100"
-                                src={cerveza4}
-                                alt="Second slide"
-                            />
+                        <Carousel.Caption >
 
-                            <Carousel.Caption>
-                                <h4>{t("who.w7")}</h4>
-                                <h4>{t("who.w8")}</h4>
-                                <h4>{t("who.w9")}</h4>
-                                <h4>{t("who.w10")}</h4>
 
-                            </Carousel.Caption>
-                        </Carousel.Item>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img
+                            className="d-block w-100"
+                            src={cerveza4}
+                            alt="Second slide"
+                        />
+
+                        <Carousel.Caption>
+
+
+                        </Carousel.Caption>
+                    </Carousel.Item>
 
 
 
 
-                        <Carousel.Item>
-                            <img
-                                className="d-block w-100"
-                                src={cerve1}
-                                alt="Third slide"
-                            />
+                    <Carousel.Item>
+                        <img
+                            className="d-block w-100"
+                            src={cerve1}
+                            alt="Third slide"
+                        />
 
-                            <Carousel.Caption>
+                        <Carousel.Caption>
 
-                                <h2>{t("who.w11")}</h2>
-                            </Carousel.Caption>
-                        </Carousel.Item>
-                    </Carousel>
-                </Col>
-                {/* </Col>
-                    <Col>
-                
-             
-                    <Card style={{border: 'none', backgroundColor: 'rgba(0,0,0,0.6)',color:'white', width:'60%'}}>
-                        <p className="text-1">{t("who.w2")}</p>
-                        <p className="text-1">{t("who.w3")}</p>
-                        <p className="text-1">{t("who.w4")}</p>
-                        <p className="text-1">{t("who.w5")}</p>
-                        <p className="text-1">{t("who.w6")}</p>
-                        <p className="text-1">{t("who.w7")}</p>
-                        <p className="text-1">{t("who.w8")}</p>
-                        <p className="text-1">{t("who.w9")}</p>
-                        <p className="text-1">{t("who.w10")}</p>
-                        <p className="text-1">{t("who.w11")}</p>
-                        
-                    
-                    </Card>
-                    </Col> */}
-            </Row>
-        </Container>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                </Carousel>
+            </Col> 
+            
+            <Col style={{padding:'0px'}} xs={12} sm={12} md={4} lg={4} xxl={4}> 
+                <Card style={{ backgroundColor: 'rgba(0,0,0,0.0)', color: 'white', border: 'none',padding:'2rem 2rem'}} className="d-flex justify-content-center" >
+
+
+                    <p className="text-1">{t("who.w2")}</p>
+                    <p className="text-1">{t("who.w3")}</p>
+                    <p className="text-1">{t("who.w4")}</p>
+                    <p className="text-1">{t("who.w5")}</p>
+                    <p className="text-1">{t("who.w6")}</p>
+                    <p className="text-1">{t("who.w7")}</p>
+                    <p className="text-1">{t("who.w8")}</p>
+                    <p className="text-1">{t("who.w9")}</p>
+                    <p className="text-1">{t("who.w10")}</p>
+                    <p className="text-1">{t("who.w11")}</p>
+
+                </Card>
+
+
+            </Col> 
+
+             </Row> 
+        // </Container>
 
 
 
